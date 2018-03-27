5939166457af66311a14bbcc8acf7503f49ebcde
quilam65 <quilt@atmarkcafe.org>
2018-03-27 18:20:20 +0700

    get product with category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
-class CateroriesController < ApplicationController
+class CategoriesController < ApplicationController
   before_action :authenticate_user!
+
   def show
+    @products = Category.find(params[:id]).products
   end
 end
