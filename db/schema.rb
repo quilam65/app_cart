d03ce4aca0c008f0fc0a1dc1c72d3d2dbdbc5d1c
quilam65 <quilt@atmarkcafe.org>
2018-03-29 20:44:50 +0700

    add calomn fisished check giao hang

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329084732) do
+ActiveRecord::Schema.define(version: 20180329112245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180329084732) do
     t.string "address"
     t.string "name"
     t.integer "phone"
+    t.boolean "finished", default: false
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
