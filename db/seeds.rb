93e3255b32c956af25017fc291f1afff7f0097e3
quilam65 <quilt@atmarkcafe.org>
2018-03-29 09:21:12 +0700

    edit view product details, get 5 category product(10)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,21 @@
 require 'open-uri'
 require 'json'
 require 'net/http'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Category.destroy_all
-Product.destroy_all
-Cart.destroy_all
-Order.destroy_all
-c = Category.create(title:'Iphone')
-
-#!/usr/bin/env ruby
-
-require 'time'
-require 'uri'
 require 'openssl'
 require 'base64'
+require 'time'
+require 'uri'
+
+Category.delete_all
+Product.delete_all
+Cart.delete_all
+Order.delete_all
+Category.create(title:'Iphone')
+Category.create(title:'Samsung')
+Category.create(title:'Sony')
+Category.create(title:'Huawai')
+Category.create(title:'LG')
+category = Category.all
 
 # Your Access Key ID, as taken from the Your Account page
 ACCESS_KEY_ID = "AKIAIONONMVS5BPD3WDQ"
@@ -32,57 +28,47 @@ ENDPOINT = "webservices.amazon.com"
 
 REQUEST_URI = "/onca/xml"
 
-params = {
-  "Service" => "AWSECommerceService",
-  "Operation" => "ItemSearch",
-  "AWSAccessKeyId" => "AKIAIONONMVS5BPD3WDQ",
-  "AssociateTag" => "storetestapp-20",
-  "SearchIndex" => "All",
-  "Keywords" => c.title.to_s,
-  "ResponseGroup" => "Images,ItemAttributes,Offers"
-}
+category.each do |cate|
+  params = {
+    "Service" => "AWSECommerceService",
+    "Operation" => "ItemSearch",
+    "AWSAccessKeyId" => "AKIAIONONMVS5BPD3WDQ",
+    "AssociateTag" => "storetestapp-20",
+    "SearchIndex" => "All",
+    "Keywords" => cate.title.to_s,
+    "ResponseGroup" => "Medium"
+  }
 
-# Set current timestamp if not set
-params["Timestamp"] = Time.now.gmtime.iso8601 if !params.key?("Timestamp")
+  # Set current timestamp if not set
+  params["Timestamp"] = Time.now.gmtime.iso8601 if !params.key?("Timestamp")
 
-# Generate the canonical query
-canonical_query_string = params.sort.collect do |key, value|
-  [URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")), URI.escape(value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))].join('=')
-end.join('&')
+  # Generate the canonical query
+  canonical_query_string = params.sort.collect do |key, value|
+    [URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")), URI.escape(value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))].join('=')
+  end.join('&')
 
-# Generate the string to be signed
-string_to_sign = "GET\n#{ENDPOINT}\n#{REQUEST_URI}\n#{canonical_query_string}"
+  # Generate the string to be signed
+  string_to_sign = "GET\n#{ENDPOINT}\n#{REQUEST_URI}\n#{canonical_query_string}"
 
-# Generate the signature required by the Product Advertising API
-signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), SECRET_KEY, string_to_sign)).strip()
+  # Generate the signature required by the Product Advertising API
+  signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), SECRET_KEY, string_to_sign)).strip()
 
-# Generate the signed URL
-request_url = "http://#{ENDPOINT}#{REQUEST_URI}?#{canonical_query_string}&Signature=#{URI.escape(signature, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
+  # Generate the signed URL
+  request_url = "http://#{ENDPOINT}#{REQUEST_URI}?#{canonical_query_string}&Signature=#{URI.escape(signature, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
 
-puts "Signed URL: \"#{request_url}\""
+  puts "Signed URL: \"#{request_url}\""
 
-@categories = Category.all
-@doc = Net::HTTP.get_response(URI.parse(request_url)).body
-@doc =  JSON.parse Hash.from_xml(@doc).to_json
-@doc = @doc['ItemSearchResponse']['Items']['Item']
-@doc.each do |json|
-  begin
-    price = json['ItemAttributes']['ListPrice']['Amount'].to_i/100
-  rescue
-     price = 10
+  @doc = Net::HTTP.get_response(URI.parse(request_url)).body
+  @doc =  JSON.parse Hash.from_xml(@doc).to_json
+  @doc = @doc['ItemSearchResponse']['Items']['Item']
+  @doc.each do |json|
+    begin
+      price = json['ItemAttributes']['ListPrice']['Amount'].to_i/100
+    rescue
+      price = 100
+    end
+    Product.create!(title: json['ItemAttributes']['Title'], price: price, category_id: cate.id, image: json['LargeImage']['URL'])
   end
-
-  begin
-    image = json['LargeImage']['URL']
-  rescue
-     image = ''
-  end
-
-  puts price
-  puts '888888888888888888888888888888888888888888888888888888888888'
-  # if !json['ItemAttributes']['ListPrice'].try(:FormattedPrice).nil?
-  puts Product.create!(title: json['ItemAttributes']['Title'], price: price, category_id: c.id, image: image)
-  #   puts p
-  # end
-  # @price.push(json['ItemAttributes']['ListPrice']['FormattedPrice'])
 end
+puts 'Get Product success!: '
+puts Product.all.count
