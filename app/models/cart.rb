d3194c3fc434df8fe1999be41bb1d421b140f30c
quilam65 <quilt@atmarkcafe.org>
2018-03-30 10:43:55 +0700

    fix history index view, status cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,7 @@
 class Cart < ApplicationRecord
   default_scope { order(created_at: :ASC) }
 
-  scope :finished, -> { where('status = ?', true) }
+  scope :finished, -> { where('status = ?', true).reverse }
   has_many :orders
   has_many :products , through: :orders
   belongs_to :user
