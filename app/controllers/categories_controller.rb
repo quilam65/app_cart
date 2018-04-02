5f44da9048a09a86219b752b6008bea8158f4e13
QuiLT <quilt@atmarkcafe.org>
2018-04-02 16:57:23 +0700

    fix routes phan trang

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,10 +3,10 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @products = Category.find(params[:id]).products.page(params[:page]).per(5)
+    @products = Category.find(params[:id]).products.page(params[:page]).per(1)
   end
 
   def index
-    @categories = Category.all.page(params[:page]).per(5)
+    @categories = Category.all.page(params[:page]).per(1)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'categories#index'
 
-  concern :paginatable do
-    get '(page/:page)', action: :index, on: :collection, as: ''
-  end
-
   resources :orders
   resources :carts do
     member do
@@ -15,11 +11,11 @@ Rails.application.routes.draw do
      get 'info'
    end
   end
-  resources :categories, concerns: :paginatable do
-    resources :products, concerns: :paginatable
+  resources :categories do
+    resources :products
   end
   get 'histories/index'
-  get 'histories/:id' => 'histories#show', concerns: :paginatable
+  get 'histories/:id' => 'histories#show'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
