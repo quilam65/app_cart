11c1316450f1af20e63869495b5cef7da0e42262
QuiLT <quilt@atmarkcafe.org>
2018-04-03 16:56:47 +0700

    add information user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   def search_cart
     if authenticate_user!
       @cart = current_user.carts.where(status: false).limit(1).first
-      @cart = Cart.create(status: false, user_id: current_user.id) if @cart.blank?
+      @cart = Cart.create(status: false, user_id: current_user.id,
+                          name: current_user.name, phone: current_user.phone,
+                          address: current_user.address) if @cart.blank?
     end
   end
 end
