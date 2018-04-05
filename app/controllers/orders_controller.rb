40652b10315acac7f64fe8e39ab5ff0d73578293
QuiLT <quilt@atmarkcafe.org>
2018-04-05 09:01:37 +0700

    add porduct to cart => login

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
-before_action :assign_params, only: [:create]
-before_action :get_order, only: [:update, :destroy]
-skip_before_action :verify_authenticity_token, :only => [:update]
-
+  before_action :assign_params, only: [:create]
+  before_action :get_order, only: [:update, :destroy]
+  skip_before_action :verify_authenticity_token, :only => [:update]
+  before_action :authenticate_user!
+  
   def create
     @order = Order.where('product_id = ? AND cart_id = ?', @order_params[:product_id], @order_params[:cart_id]).limit(1).first
     if @order.present?
