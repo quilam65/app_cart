e5a8dedac27b04bc62e9b5d8f8d1b5d32bc5b79e
QuiLT <quilt@atmarkcafe.org>
2018-04-05 11:23:18 +0700

    change env

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,3 +1,3 @@
-PayPal::SDK::Core::Config.load('config/paypal.yml',  ENV['RACK_ENV'] || 'development')
+PayPal::SDK::Core::Config.load('config/paypal.yml',  ENV['RACK_ENV'] || 'production')
 # PayPal::SDK.logger = Rails.logger
 PayPal::SDK.logger.level = Logger::INFO
