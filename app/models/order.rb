9dc7ddf0cb83aa4288d56cf9aae6495a416cadae
QuiLT <quilt@atmarkcafe.org>
2018-04-06 15:43:02 +0700

    fix sql admin

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,8 @@ class Order < ApplicationRecord
   validates :quanlity, presence: true
   validates :quanlity, numericality: { greater_than: 0 }
 
+  scope :finished_cart, -> { joins(:cart).where('carts.status = true') }
+
   def self.find_order(product_id, cart_id)
     return Order.where('product_id = ? AND cart_id = ?',product_id, cart_id).limit(1).first
   end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -65,6 +65,7 @@ RailsAdmin.config do |config|
 
   config.model 'Order' do
     list do
+      scopes [:finished_cart]
       field :cart do
         searchable ["cart.name"]
       end
@@ -107,7 +108,7 @@ RailsAdmin.config do |config|
         searchable ["category.title"]
       end
       field :title
-      field :description
+      field :description, :ck_editor
       field :price
     end
 
