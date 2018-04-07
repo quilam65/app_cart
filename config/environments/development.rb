b5d1cb98198ffadd3c4a41cf1415ea0dd83e732c
QuiLT <quilt@atmarkcafe.org>
2018-04-07 16:48:00 +0700

    Revert "Revert "fix config mail""
    This reverts commit a063421cc03627dc5cb1a81dc4bb94fc235cf0cf.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
       port: 587,
       domain:  'domain.com',
       user_name: 'anhqui1995@gmail.com',
-      password:  ENV["PASS_MAIL"],
+      password:  'Arduinobegin1995@',
       authentication: 'plain',
       enable_starttls_auto: true
     }
